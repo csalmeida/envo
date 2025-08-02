9af7573134bbbbaf89ce8458f65b79d38be8846a
Cristiano <cs.almeida@protonmail.ch>
2025-08-02 18:18:01 -0500

    Exposes module as a library.

--- build.zig
+++ build.zig
@@ -15,6 +15,13 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    // Exposes module as a library to be used by other projects.
+    _ = b.addModule("envo", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+     });
+
     // This creates a "module", which represents a collection of source files alongside
     // some compilation options, such as optimization mode and linked system libraries.
     // Every executable or library we compile will be based on one or more modules.
