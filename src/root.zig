e24d57f8ece8dec4826513c16982e488692effe7
Cristiano <cs.almeida@protonmail.ch>
2025-08-03 11:33:02 -0500

    Changed 1mb definition.

--- src/root.zig
+++ src/root.zig
@@ -8,7 +8,7 @@ pub fn loadFile(allocator: Allocator, file_path: []const u8) ![]u8 {
   const cwd = std.fs.cwd();
 
   // To read all the file we need to define the max size for it, pass the path where to look for the file and allocate it to memory.
-  const max_file_size: usize = 1_000_000; // 1mb.
+  const max_file_size: usize = 1024 * 1024; // 1mb.
   const contents = try cwd.readFileAlloc(allocator, file_path, max_file_size);
 
   return contents;
